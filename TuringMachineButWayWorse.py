030131cc538da03f8997bfa4e91c79c1a350af22
leonardoInf <mondloslp@gmail.com>
2019-10-13 11:48:53 +0200

    Added option to read .tape file without piping (sys.argv[2])

--- TuringMachineButWayWorse.py
+++ TuringMachineButWayWorse.py
@@ -1,4 +1,5 @@
 import sys
+import os
 
 neg_tape = []
 tape = []
@@ -6,35 +7,46 @@ pointer = 0
 state = '0'
 filename = ""
 
-if not sys.stdin.isatty():
-    tape = list(map(ord, sys.stdin.read()))
-    if(tape[0] == 65279):       #remove BOM if present
-        tape = tape[1:]
-
 try:
     filename = sys.argv[1]
 except:
     exit("Error: no .tmw file has been provided")
+
+if not sys.stdin.isatty():      # try to read tape from piped stdin
+    tape = list(map(ord, sys.stdin.read()))
+    if tape[0] == 65279:       # remove BOM if present
+        tape = tape[1:]
+else:                           # else try to read tape from file
+    try:
+        tapeFilename = sys.argv[2]
+        with open(tapeFilename, "r") as file:
+            tape = list(map(ord, file.read()))
+    except:
+        if tapeFilename != "":
+            exit("Tape file '{}' does not exist or is corrupt".format(tapeFilename))
     
-lookup = {} 
-try:   
-    with open(filename, "r") as file:
-        s = file.readline()
-        while s:
-            i = list(s.split())
-            i[0], i[2], i[3], i[5], i[6] =  int(i[0]), int(i[2]), int(i[3]), int(i[5]), int(i[6])
-            if len(i) != 7:
-                raise Exception("Expected 7 fields, found %s" % len(i))
-            if i[0] != 0 and i[0] != 1:
-                raise Exception("Invalid tape current value %s, expected 0 or 1" % i[0])
-            if i[2] != 0 and i[2] != 1:
-                raise Exception("Invalid tape next value %s, expected 0 or 1" % i[2])
-            if i[3] != 0 and i[3] != 1:
-                raise Exception("Invalid movement direction %s, expected 0 (left) or 1 (right)" % i[3])
-            lookup[(i[0], str(i[1]))] = tuple(i[2:])
+lookup = {}
+if os.path.isfile(filename):    #check if file exists
+    try:   
+        with open(filename, "r") as file:
             s = file.readline()
-except:
-    exit("Could not read file '{}'".format(filename))
+            while s:
+                i = list(s.split())
+                i[0], i[2], i[3], i[5], i[6] =  int(i[0]), int(i[2]), int(i[3]), int(i[5]), int(i[6])
+                if len(i) != 7:
+                    raise Exception("Expected 7 fields, found %s" % len(i))
+                if i[0] != 0 and i[0] != 1:
+                    raise Exception("Invalid tape current value %s, expected 0 or 1" % i[0])
+                if i[2] != 0 and i[2] != 1:
+                    raise Exception("Invalid tape next value %s, expected 0 or 1" % i[2])
+                if i[3] != 0 and i[3] != 1:
+                    raise Exception("Invalid movement direction %s, expected 0 (left) or 1 (right)" % i[3])
+                lookup[(i[0], str(i[1]))] = tuple(i[2:])
+                s = file.readline()
+    except:
+        exit("Could not read file '{}'".format(filename))
+else:
+        exit("File '{}' does not exist".format(filename))
 
 while True:
     debug_tape = "".join(map(lambda c: bin(256 | c)[3:], neg_tape[::-1])) + "".join(map(lambda c: bin(256 | c)[3:], tape))
